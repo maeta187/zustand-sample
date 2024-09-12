8751bb77245f542de41ad277a52cab8374a3decd
maeta187 <nexus18789@gmail.com>
2024-09-12 21:28:43 +0900

    ✨ : selectorのまとめ

---
+++ src/app/selector/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+import { Selector } from './selector'
+
+const Page: NextPage = () => {
+  return (
+    <div>
+      <h1>Selector</h1>
+      <Selector />
+    </div>
+  )
+}
+
+export default Page
