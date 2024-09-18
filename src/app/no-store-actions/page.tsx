f7daf8859bf261fbc0372f6081753d594bcc5857
maeta187 <nexus18789@gmail.com>
2024-09-18 20:54:52 +0900

    ✨ : NoStoreActionsのまとめ

---
+++ src/app/no-store-actions/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+import { NoStoreActions } from './NoStoreActions'
+
+const Page: NextPage = () => {
+  return (
+    <div>
+      <h1>No-Store-Actions</h1>
+      <NoStoreActions />
+    </div>
+  )
+}
+
+export default Page
