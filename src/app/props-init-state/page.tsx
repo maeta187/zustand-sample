a313c73cd9a54f853621171ba1f3edf0fd9c0042
maeta187 <nexus18789@gmail.com>
2024-10-12 13:36:34 +0900

    🎨 : コンポーネントの追加

--- src/app/props-init-state/page.tsx
+++ src/app/props-init-state/page.tsx
@@ -2,8 +2,14 @@
 
 import { useRef } from 'react'
 import { NextPage } from 'next'
-import { createBearStore, BearContext } from '@/context/bear-context'
+import { BearContext, createBearStore } from '@/context/bear-context'
+import { BearProvider } from '@/context/common-bear-context'
 import { BasicConsumer } from './BasicConsumer'
+import { CommonConsumer } from './CommonConsumer'
+
+const bearProps = {
+  bears: 5
+}
 
 /**
  * Contextを使用することでコンポーネント特定のコンポーネントから状態を参照できるようになる。
@@ -15,12 +21,27 @@ import { BasicConsumer } from './BasicConsumer'
 const Page: NextPage = () => {
   const store = useRef(createBearStore()).current
   return (
-    <BearContext.Provider value={store}>
+    <div>
+      <BearContext.Provider value={store}>
+        <h1 className='text-3xl'>Props-Init-State</h1>
+        <div>
+          <h2 className='text-2xl'>Basic</h2>
+          <BasicConsumer title={'Basic Consumer'} />
+        </div>
+      </BearContext.Provider>
       <div>
-        <h1>Props-Init-State</h1>
-        <BasicConsumer />
+        <h2 className='text-2xl'>Common</h2>
+        <BearProvider {...bearProps}>
+          <BasicConsumer title={'Common Consumer'} />
+        </BearProvider>
       </div>
-    </BearContext.Provider>
+      <div>
+        <h2 className='text-2xl'>useBearContext</h2>
+        <BearProvider {...bearProps}>
+          <CommonConsumer />
+        </BearProvider>
+      </div>
+    </div>
   )
 }
 
