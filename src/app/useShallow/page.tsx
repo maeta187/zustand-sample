f359f2f6f46392c9412374f32e51faa50840bf5f
maeta187 <nexus18789@gmail.com>
2024-10-22 22:07:14 +0900

    ✨ : useShallowまとめ

---
+++ src/app/useShallow/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+import { BearNames } from './BearNames'
+
+const Page: NextPage = () => {
+  return (
+    <div>
+      <h1>useShallow</h1>
+      <BearNames />
+    </div>
+  )
+}
+
+export default Page
